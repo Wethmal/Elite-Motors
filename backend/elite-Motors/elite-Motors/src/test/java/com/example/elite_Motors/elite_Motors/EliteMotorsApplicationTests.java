9c57daa11c908a2a11584b115ddd654d07fae82b
Wethmal <kisanduofficially@gmail.com>
2026-01-14 00:15:06 +0530

    spring boot project inisilized
    succuess ✏️

---
+++ backend/elite-Motors/elite-Motors/src/test/java/com/example/elite_Motors/elite_Motors/EliteMotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elite_Motors.elite_Motors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EliteMotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
